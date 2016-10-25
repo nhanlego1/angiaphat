4fb30e8f2a26e4d4a55134662ec10cc8fcc3dc72
conghau <conghau.master1@gmail.com>
2016-10-25 20:57:19 +0700

    Update

--- sites/all/themes/angiaphat/templates/views-view-fields--dich-vu.tpl.php
+++ sites/all/themes/angiaphat/templates/views-view-fields--dich-vu.tpl.php
@@ -6,13 +6,18 @@
  * Time: 17:43
  */
 ?>
-<img src="<?php print @image_style_url('medium', agp_getValue($row, 'field_field_image.0.raw.uri')) ?>"
-  alt="<?php print @agp_getValue($row, 'node_title') ?>" title="<?php print @agp_getValue($row, 'node_title') ?>" class="img_inner fleft">
-<div class="extra_wrapper">
-  <div class="text1">
-    <a
-      href="<?php print url('node/' . $row->nid) ?>"><?php print @agp_getValue($row, 'node_title') ?></a>
+<div class="agp-service-item">
+  <img
+    src="<?php print @image_style_url('medium', agp_getValue($row, 'field_field_image.0.raw.uri')) ?>"
+    alt="<?php print @agp_getValue($row, 'node_title') ?>"
+    title="<?php print @agp_getValue($row, 'node_title') ?>"
+    class="img_inner fleft">
+  <div class="extra_wrapper">
+    <div class="text1">
+      <a
+        href="<?php print url('node/' . $row->nid) ?>"><?php print @agp_getValue($row, 'node_title') ?></a>
+    </div>
+    <?php print @agp_getValue($row, 'field_body.0.raw.summary') ?>
   </div>
-  <?php print @agp_getValue($row, 'field_body.0.raw.summary') ?>
+  <div class="clear cl1"></div>
 </div>
-<div class="clear cl1"></div>
