b8b9df5bada56d2e045f9ebd3fb0ea9b8bf2b510
Nhan Le <nhan.lq@mobivi.vn>
2016-12-06 16:15:13 +0700

    update

--- sites/all/themes/angiaphat/templates/views-view-fields--san-pham.tpl.php
+++ sites/all/themes/angiaphat/templates/views-view-fields--san-pham.tpl.php
@@ -5,12 +5,11 @@
  * Date: 24/10/2016
  * Time: 15:10
  */
+
 ?>
 <div class="grid_4">
   <div class="box">
-    <img
-      src="<?php print @file_create_url(agp_getValue($row, 'field_field_images.0.raw.uri')) ?>"
-      alt="">
+    <?php print $fields['field_images']->content ?>
     <div class="box_bot">
       <div class="maxheight" style="height: 70px;">
         <div class="box_inner">
@@ -18,7 +17,7 @@
             <a
               href="<?php print url('node/' . $row->nid) ?>"><?php print (isset($row->node_title)) ? $row->node_title : 'N/A' ?></a>
             <div
-              class="col1 txf"><?php print @agp_getValue($row, 'field_field_price.0.rendered.#markup', 'N/A') ?></div>
+              class="col1 txf"><?php print $fields['field_price']->content ?></div>
           </div>
         </div>
       </div>
